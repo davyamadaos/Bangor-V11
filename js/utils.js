3e916d5925af11599e1e2b1fd20ec1d47df858ab
davyamadaos <dfmapping@gmail.com>
2026-06-24 07:30:07 +0100

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,44 +1,14 @@
-export function formatLevel(level) {
-
-    return Number(level)
-        .toFixed(2) + " m";
+export function fmtLevel(v) {
+    return Number(v).toFixed(2) + " m";
 }
 
-export function formatGauge(value) {
-
-    return "Gauge " +
-        Number(value)
-        .toFixed(2);
+export function fmtGauge(v) {
+    return "Gauge " + Number(v).toFixed(2);
 }
 
-export function formatRate(rate) {
-
-    return Math.abs(rate)
-        .toFixed(1) +
-        " cm/hr";
-}
-
-export function formatTime(dateString) {
-
-    const d = new Date(dateString);
-
-    return d.toLocaleTimeString(
-        "en-IE",
-        {
-            hour: "2-digit",
-            minute: "2-digit"
-        }
-    );
-}
-
-export function hoursOld(dateString) {
-
-    const then = new Date(dateString);
-
-    const now = new Date();
-
-    return (
-        (now - then)
-        / 3600000
-    ).toFixed(1);
+export function fmtTime(t) {
+    return new Date(t).toLocaleTimeString("en-IE", {
+        hour: "2-digit",
+        minute: "2-digit"
+    });
 }
