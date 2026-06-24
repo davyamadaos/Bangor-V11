efb59f7994ae5ca0da2268d2a1aa665f521851ea
davyamadaos <dfmapping@gmail.com>
2026-06-24 11:39:39 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,15 +1,21 @@
 export const CONFIG = {
+
     station: "33008",
 
     epaImage:
         "https://epawebapp.epa.ie/hydronet/output/internet/stations/CAS/33008/S/extralarge_3m_extralarge.png",
 
     gaugeMultiplier: 16.921,
+
     gaugeOffset: 1675.7,
 
     refreshMinutes: 15
 };
 
 export function toGauge(level) {
-    return CONFIG.gaugeMultiplier * level - CONFIG.gaugeOffset;
+
+    return (
+        CONFIG.gaugeMultiplier * Number(level)
+        - CONFIG.gaugeOffset
+    );
 }
